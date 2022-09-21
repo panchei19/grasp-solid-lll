924facb9bc31665bd6da5762d19f2d5e5bf5ff95
panchei19 <110833799+panchei19@users.noreply.github.com>
2022-09-21 10:54:07 -0300

    Add files via upload

---
+++ grasp-solid-lll/src/Library/printer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class printer
+    {
+        public void OnlyPrinter(List<>)
+        {
+
+        }
+    }
+}
